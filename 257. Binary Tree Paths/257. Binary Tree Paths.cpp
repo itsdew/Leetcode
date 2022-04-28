a9b0f15b373fded512fa9283c2f21773178dcda4
Devendra Uraon <devendra123world@gmail.com>
2022-04-28 21:55:23 +0530

    Add files via upload

---
+++ 257. Binary Tree Paths/257. Binary Tree Paths.cpp
@@ -0,0 +1,21 @@
+//Devendra Uraon 
+class Solution {
+public:
+    vector<string> ans;
+    void fun(TreeNode* root,string s)
+    {
+        if(!root->left && !root->right)
+            ans.push_back(s);
+        if(root->left)
+            fun(root->left,s+"->"+to_string(root->left->val));
+        if(root->right)
+            fun(root->right,s+"->"+to_string(root->right->val));
+    }
+    
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string s="";
+        s+=to_string(root->val);
+        fun(root,s);
+        return ans;
+    }
+};
---
+++ 257. Binary Tree Paths/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/binary-tree-paths/">257. Binary Tree Paths</a>
