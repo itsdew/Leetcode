b32bdefd78bf14bb80f941aacd1169ff9821bd3e
Devendra Uraon <devendra123world@gmail.com>
2022-05-14 15:40:32 +0530

    Add files via upload

---
+++ 766. Toeplitz Matrix/766. Toeplitz Matrix.cpp
@@ -0,0 +1,18 @@
+//Devendra Uraon
+class Solution {
+public:
+    bool isToeplitzMatrix(const vector<vector<int>>& m) {
+        if (m.empty()) {
+            return true;
+        }
+        const auto n = m[0].size() - 1;
+        for (auto i = 1u; i < m.size(); ++i) {
+            const auto b1 = m[i - 1].begin();
+            const auto b2 = m[i].begin();
+            if (!equal(b1, b1 + n, b2 + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
---
+++ 766. Toeplitz Matrix/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/toeplitz-matrix/">766. Toeplitz Matrix</a>
