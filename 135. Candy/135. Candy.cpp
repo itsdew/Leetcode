257f9d5f05caedc7d765ee28c3ceb29ec0ecd490
Devendra Uraon <devendra123world@gmail.com>
2022-07-05 01:40:53 +0530

    Add files via upload

---
+++ 135. Candy/135. Candy.cpp
@@ -0,0 +1,32 @@
+//Devendra Uraon
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum=1, height=0, heightVal=INT_MAX, current=1;
+        for(int i=1;i<ratings.size();i++){
+            if(ratings[i]>ratings[i-1]){
+                current++;
+                heightVal=current;
+                height=i;
+                sum+=current;
+            }
+            else if(ratings[i]<ratings[i-1]){
+                int count=1;
+                if((current==1) ? count=i-height+1 : height=i);
+                current=1;
+                sum+=count;
+                if(count==heightVal)
+                {
+                    heightVal++; sum++;
+                }
+            }
+            else{
+                current=1;
+                heightVal=INT_MAX;
+                height=i;
+                sum+=current;
+            }
+        }
+        return sum;
+    }
+};
---
+++ 135. Candy/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/candy/">135. Candy</a>
