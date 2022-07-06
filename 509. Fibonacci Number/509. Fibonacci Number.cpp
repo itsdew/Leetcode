c513fc7b3d97b3ea496295edb81eb33c1c379dac
Devendra Uraon <devendra123world@gmail.com>
2022-07-06 19:50:58 +0530

    Add files via upload

---
+++ 509. Fibonacci Number/509. Fibonacci Number.cpp
@@ -0,0 +1,13 @@
+//Devendra Uraon
+class Solution {
+    public:
+    int fib(int N) {
+    int f[N+2], i; 
+    f[0] = 0, f[1] = 1; 
+
+    for (i = 2; i <= N; i++){ 
+            f[i] = f[i-1] + f[i-2]; 
+        }
+    return f[N]; 
+    }
+};
---
+++ 509. Fibonacci Number/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/fibonacci-number/">509. Fibonacci Number</a>
