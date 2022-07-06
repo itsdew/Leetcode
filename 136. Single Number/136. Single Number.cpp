679090419f073dd550bb5cc3ec58d021768fb2bb
Devendra Uraon <devendra123world@gmail.com>
2022-07-06 19:45:36 +0530

    Add files via upload

---
+++ 136. Single Number/136. Single Number.cpp
@@ -0,0 +1,12 @@
+//Devendra Uraon
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sol = 0, T = nums.size(), i = 0;
+        while(T--){
+            sol = nums[i]^sol;
+            ++i;
+        }
+        return sol;
+    }
+};
---
+++ 136. Single Number/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/single-number/">136. Single Number</a>
