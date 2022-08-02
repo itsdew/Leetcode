68214a128da3989c545996771c76de291c8f0f23
Devendra Uraon <devendra123world@gmail.com>
2022-08-02 22:42:22 +0530

    Add files via upload

---
+++ 62. Unique Paths/62. Unique Paths.cpp
@@ -0,0 +1,24 @@
+//Author: Devendra Uraon
+class Solution {
+public:    
+    int uniquePaths(int m, int n) {
+        double num = 1, div = 1;
+        int max, min;
+        
+        if (m > n){
+            max = m - 1;
+            min = n - 1;
+        }
+        else{
+            max = n - 1;
+            min = m - 1;
+        }
+        
+        for(int i = max + min; i > max; i--)
+            num *= i;
+        for (int i = 1; i <= min; i++)
+            div *= i;
+
+        return num / div;
+    }
+};
---
+++ 62. Unique Paths/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/unique-paths/">62. Unique Paths</a>
