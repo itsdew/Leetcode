d61f2e11aa86a37e2fc5ea4967d6b7d8520aa2a2
Devendra Uraon <devendra123world@gmail.com>
2022-08-07 00:02:09 +0530

    Add files via upload

---
+++ 29. Divide Two Integers/29. Divide Two Integers.cpp
@@ -0,0 +1,23 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        //bitwise
+        long long n = dividend;
+        long long m = divisor;
+        int res = n<0 ^ m<0 ? -1 : 1;
+        n = abs(n);
+        m = abs(m);
+        long long q=0, t=0;
+        for(long long i = 31; i>=0; i--){ 
+            if(t + (m<<i) <= n ){
+                t += m<<i;
+                q = q | (1ll<<i);
+            }
+        }
+        if(res < 0) q = -q;
+        if(q < INT_MIN) return INT_MIN;
+        if(q >= INT_MAX) return INT_MAX;
+        return q;
+    }
+};
---
+++ 29. Divide Two Integers/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/divide-two-integers/">29. Divide Two Integers</a>
