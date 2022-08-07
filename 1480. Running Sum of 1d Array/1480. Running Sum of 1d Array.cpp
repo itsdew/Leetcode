35b63e3dfa2128c7e883799b22d7326c38e10bb9
Devendra Uraon <devendra123world@gmail.com>
2022-08-07 23:12:59 +0530

    Add files via upload

---
+++ 1480. Running Sum of 1d Array/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,13 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> solve;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            solve.push_back(sum);
+        }
+        return solve;
+    }
+};
---
+++ 1480. Running Sum of 1d Array/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/running-sum-of-1d-array/">1480. Running Sum of 1d Array</a>
