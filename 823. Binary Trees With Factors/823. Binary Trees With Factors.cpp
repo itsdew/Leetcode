a3b996e1d63436ddc93399eef790a27c895d95fd
Devendra Uraon <devendra123world@gmail.com>
2022-08-09 23:22:40 +0530

    Add files via upload

---
+++ 823. Binary Trees With Factors/823. Binary Trees With Factors.cpp
@@ -0,0 +1,21 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int numFactoredBinaryTrees(vector<int>& A) {
+        sort(A.begin(), A.end());
+        const long Mod = 1e9+7;        
+        unordered_map<int, long> dp;
+        for(int i = 0; i < A.size(); i++){
+            dp[A[i]] = 1;
+            for(int j = 0; j < i; j++){ // A[i] = A[j] *  A[i]/A[j]
+                if(A[i] % A[j] ==0 && dp.count(A[i] / A[j])){
+                    dp[A[i]] += (dp[A[j]] * dp[A[i] / A[j]]) % Mod; 
+                }
+            }
+        }
+        long ans = 0;
+        for(auto& i: dp)
+            ans = (ans + i.second) % Mod;
+        return ans % Mod;
+    }
+};
---
+++ 823. Binary Trees With Factors/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/binary-trees-with-factors/">823. Binary Trees With Factors</a>
