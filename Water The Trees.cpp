db677978dfdcc789ff961cc12aad85471d0344bb
Devendra Uraon <devendra123world@gmail.com>
2022-08-11 04:17:54 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,75 +1,60 @@
 //Question: https://codeforces.com/contest/1661/problem/C
-//Author: Devendra Uraon
+// 2022-03-14
 #include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
 #define pi pair<int, int>
 #define vp vector<pi>
 #define ll long long
-#define forloop for(ll i = 0; i < n; ++i)
-#define input cin >>
-#define print cout <<
 #define MAX 2147000000
 #define MOD 998244353LL
 using namespace std;
- 
+
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	ll T; cin >> T;
-    while(T--){
-        ll n; input n;
-        vl vec(n);
-        ll mx = 0;
-        forloop{
-            input vec[i];
-            mx = max(mx, vec[i]);
+    fastio;
+    int n;
+    cin >> n;
+    vi C(n);
+    for(int i{0}; i < n; ++i){
+        cin >> C[i];
+    }
+    vector<string> vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i];
+    }
+    vector<vl> dp(n, vl(2, MAX));
+    dp[0][0] = 0;
+    dp[0][1] = C[0];
+    for(int i{1}; i < n; ++i){
+        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
+            cout << -1;
+            return 0;
         }
-        ll l = 0, r = LLONG_MAX;
-        ll ans = LLONG_MAX;
-        while(l <= r){
-            ll m = (l + r) / 2;
-            ll one = (m + 1) / 2;
-            ll two = m / 2;
-            vl v;
-            forloop{
-                v.push_back(mx - vec[i]);
-            }
-            forloop{
-                ll k = v[i] / 2;
-                v[i] -= min(k, two) * 2;
-                two -= min(k, two);
-            }
-            if(one >= accumulate(v.begin(), v.end(), 0LL)){
-                r = m - 1;
-                ans = min(ans, m);
-            }
-            else{
-                l = m + 1;
-            }
+        string p = vec[i - 1];
+        string pr = p;
+        reverse(pr.begin(), pr.end());
+        string r = vec[i];
+        reverse(r.begin(), r.end());
+        if(vec[i] >= p){
+            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
         }
-        l = 0, r = LLONG_MAX;
-        while(l <= r){
-            ll m = (l + r) / 2;
-            ll one = (m + 1) / 2;
-            ll two = m / 2;
-            vl v;
-            forloop{
-                v.push_back(mx + 1 - vec[i]);
-            }
-            forloop{
-                ll k = v[i] / 2;
-                v[i] -= min(k, two) * 2;
-                two -= min(k, two);
-            }
-            if(one >= accumulate(v.begin(), v.end(), 0LL)){
-                r = m - 1;
-                ans = min(ans, m);
-            }
-            else{
-                l = m + 1;
-            }
+        if(vec[i] >= pr){
+            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
         }
-        print ans << "\n";
+        if(r >= p){
+            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
+        }
+        if(r >= pr){
+            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
+        }
+    }
+    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
+    if(ans == MAX) cout << -1;
+    else{
+        cout << ans;
     }
 }
