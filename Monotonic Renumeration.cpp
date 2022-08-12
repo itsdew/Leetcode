6fbded75fb606a6abd5199132aa68c8f81029605
Devendra Uraon <devendra123world@gmail.com>
2022-08-12 21:44:00 +0530

    Update Monotonic Renumeration.cpp

--- Monotonic Renumeration.cpp
+++ Monotonic Renumeration.cpp
@@ -1,67 +1,77 @@
-//Author: Devendra Uraon
-#include <bits/stdc++.h>
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define pi pair<int, int>
-#define vp vector<pi>
-#define ll long long
-#define forloop for(int i = 0; i < n; ++i)
-#define input cin >>
-#define print cout <<
-#define MAX 2147000000
-#define MOD 998244353LL
-using namespace std;
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<math.h>
+#include<unordered_map>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_set>
+#include<iomanip>
 
-ll modpow(ll a, ll b){
-    ll ans = 1;
-    while(b > 0){
-        if(b % 2){
-            ans *= a;
-            ans %= MOD;
-        }
-        a *= a;
-        b /= 2;
-        a %= MOD;
-    }
-    return ans;
+#define ll long long
+#define ull unsigned long long
+#define mp make_pair
+#define ff first
+#define ss second
+#define mloop(i) for(auto i=m.begin();i!=m.end();i++)
+#define sloop(i) for(auto i=s.begin();i!=s.end();i++)
+#define pb push_back
+
+using namespace std;
+vector<bool> prime(200002,true);
+void seive(long mx)
+{
+	prime[0]=prime[1]=false;
+	for(long i=2;i<=mx;i++)
+	{
+		for(long j=2;(ll)j*j<=i;j++)
+		{
+			if(i%j == 0)
+			{
+				prime[i]=false;
+				break;
+			}
+		}
+	}
 }
- 
-int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	int n; input n;
-    vi vec(n);
-    map<int, vi> m;
-    forloop{
-        input vec[i];
-        m[vec[i]].push_back(i);
-    }
-    vp v;
-    for(auto& i : m){
-        if(i.second.size() == 1) continue;
-        auto& s = i.second;
-        sort(s.begin(), s.end());
-        v.push_back({s.front(), s.back()});
-    }
-    int sum = 0;
-    sort(v.begin(), v.end());
-    int l = -1, r = -1, cnt = 1;
-    if(v.size()){
-        l = v[0].first;
-        r = v[0].second;
-    }
-    for(int i = 0; i < (int)v.size(); ++i){
-        if(v[i].first > r){
-            sum += r - l + 1;
-            l = v[i].first;
-            r = v[i].second;
-            cnt++;
-        }
-        else{
-            //l = min(l, v[i].first);
-            r = max(r, v[i].second);
-        }
-    }
-    sum += r - l + 1;
-    print modpow(2, n - 1 - sum + cnt);
+ll power(ll i,int p)
+{
+	ll ans=1;
+	while(p)
+	{
+		if(p & 1)
+			ans = ans*i;
+		i = i*i;
+		p/=2;
+	}
+	return ans;
+}
+
+
+int main()
+{
+	ll n,q;
+	cin>>n;
+	ll arr[n];
+	for(ll i=0;i<n;i++)
+		cin>>arr[i];
+	cin>>q;
+	while(q--)
+	{
+		ll l,r,c=0,d=0,a=0,b=0;
+		cin>>l>>r;
+		c = *min_element(arr+l,arr+r+1);
+		d = *max_element(arr+l,arr+r+1);
+		if(l>0)
+		a = *max_element(arr,arr+l);
+		if(r<n-1)
+		b = *max_element(arr+r+1,arr+n);
+		float ans1 = (c+d)/2;
+		float ans2 = (c+max(a,b));
+		cout<<fixed<<std::setprecision(1)<<max(ans1,ans2)<<endl;
+	}
+	return 0;
 }
