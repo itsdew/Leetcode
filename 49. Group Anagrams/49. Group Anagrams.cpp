e405351be8781208d404206dc4a1fd914f9b8732
Devendra Uraon <devendra123world@gmail.com>
2022-08-12 22:37:48 +0530

    Add files via upload

---
+++ 49. Group Anagrams/49. Group Anagrams.cpp
@@ -0,0 +1,19 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> m;
+        for(auto it:strs){
+            string temp = it;
+            sort(temp.begin(), temp.end());
+            m[temp].push_back(it);
+        }
+        
+        vector<vector<string>> ans;
+        for(auto [temp,vec]:m)
+            ans.push_back(vec);
+        
+        return ans;
+    }
+};
---
+++ 49. Group Anagrams/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/group-anagrams/">49. Group Anagrams</a>
