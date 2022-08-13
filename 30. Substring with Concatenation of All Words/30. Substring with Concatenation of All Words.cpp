50078326b9b37a2cdf75f2b22c2d70ef7b0d924b
Devendra Uraon <devendra123world@gmail.com>
2022-08-13 23:49:58 +0530

    Add files via upload

---
+++ 30. Substring with Concatenation of All Words/30. Substring with Concatenation of All Words.cpp
@@ -0,0 +1,37 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int n = s.length();
+        int wordSize = words[0].length();
+        vector<int> ans;
+        
+        if(n<wordSize*words.size()) return ans;
+        
+        unordered_map<string, int> hash;
+        
+        for(int i = 0; i <= n-wordSize*words.size(); i++){
+            for(int k = 0; k<words.size(); k++)
+                hash[words[k]] = 0;
+            
+            for(int k = 0; k<words.size(); k++)
+                hash[words[k]]++;
+            
+            string temp = s.substr(i, wordSize*words.size());
+            
+            for(int j = 0; j<=temp.size() - wordSize; j+=wordSize){
+                string tempSub = temp.substr(j, wordSize);
+                    hash[tempSub]--;
+            }
+            int flag = 0;
+            for(int k = 0; k<words.size(); k++){
+                if(hash[words[k]] != 0){
+                    flag = 1;
+                }
+            }
+            if(flag == 0)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
---
+++ 30. Substring with Concatenation of All Words/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/substring-with-concatenation-of-all-words/">30. Substring with Concatenation of All Words</a>
