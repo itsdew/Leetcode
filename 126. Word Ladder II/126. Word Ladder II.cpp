d4cce3cbf45fa0b8dfc2079f5d5a41393b3c3446
Devendra Uraon <devendra123world@gmail.com>
2022-08-14 23:13:41 +0530

    Add files via upload

---
+++ 126. Word Ladder II/126. Word Ladder II.cpp
@@ -0,0 +1,58 @@
+//Author: Devendra Uraon - TLE
+//Helper: TaniiaRebello - TLE.Eliminator
+class Solution {
+public:
+    map<string,vector<string>> parent;
+    queue<string> q;
+    vector<string> path;
+    vector<vector<string>> ans;
+    unordered_map<string,int> distance;
+    void location(string str,vector<string> &path){
+         if(str=="0"){
+              ans.push_back(path);
+              return;
+         }
+         for(string node : parent[str]){
+              path.push_back(str);
+              location(node,path);
+              path.pop_back();
+         }
+    }
+    vector<vector<string>> findLadders(string first, string last, vector<string> &list){
+         for(string node : list) distance[node] = INT_MAX , parent[node] = {};
+         parent[first] = {"0"};
+         distance[first] = 1;
+         q.push(first);
+         while(!q.empty()){
+             string par = q.front();
+             q.pop();
+             if(par==last) break;
+             for(int i=0; i<par.size(); i++){
+                 string pref = "", suff = "";
+                 if(i) pref = par.substr(0, i);
+                 if(i<par.size()-1) suff = par.substr(i+1);
+                 string child = pref+'$'+suff;
+                 int pos = pref.size();
+                 for(char c='a'; c<='z'; c++){
+                       if(c!=par[i]){
+                            child[pos]=c;
+                            if(distance.count(child)){
+                                 if(distance[child] > 1 + distance[par]){
+                                     distance[child] =  1 + distance[par];
+                                     parent[child].clear();
+                                     parent[child].push_back(par);
+                                     q.push(child);
+                                 }
+                                 else if(distance[child] == 1 + distance[par]){
+                                     parent[child].push_back(par);
+                                 }
+                            }
+                       }
+                 }
+             }
+         }
+         location(last, path);
+         for(int i=0; i<ans.size(); i++) reverse(ans[i].begin(), ans[i].end());
+         return ans;
+    }
+};
---
+++ 126. Word Ladder II/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/word-ladder-ii/">126. Word Ladder II</a>
