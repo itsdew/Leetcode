8e03b5bca1b2df7e7daa5b8b8f2c2dc557e402ff
Devendra Uraon <devendra123world@gmail.com>
2022-08-17 20:30:54 +0530

    Add files via upload

---
+++ 804. Unique Morse Code Words/804. Unique Morse Code Words.cpp
@@ -0,0 +1,18 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector a = {".-","-...","-.-.","-..",".","..-.","--.","....",
+                    "..",".---","-.-",".-..","--","-.","---",".--.",
+                    "--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        unordered_map<string,int> um;
+
+        for (int i=0; i< words.size(); i++) {
+            string st;
+            for (int j=0; j<words[i].size(); j++) st += a[words[i][j] - 'a'];
+            um[st]=1;
+         }
+        return um.size ();
+        }
+};
---
+++ 804. Unique Morse Code Words/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/unique-morse-code-words/">804. Unique Morse Code Words</a>
