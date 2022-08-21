c5e0dcb409fcf59c6e4e430f43fffbf074a2e957
Devendra Uraon <devendra123world@gmail.com>
2022-08-21 23:39:56 +0530

    Add files via upload

---
+++ 242. Valid Anagram/242. Valid Anagram.cpp
@@ -0,0 +1,22 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       int alphabet[26]={0};
+	    for(int i = 0; i < s.size(); i++){
+		    alphabet[s[i]-'a']++;
+	    }
+        int alphabet1[26]={0};
+	    for(int i = 0; i < t.size(); i++){
+		    alphabet1[t[i]-'a']++;
+	    }
+        bool flag = 1;
+        for(int i = 0; i<26; i++){
+	    if(alphabet[i] != alphabet1[i]){
+	        flag = 0;
+	        break;
+	    }
+	}
+    return flag;
+    }
+};
---
+++ 242. Valid Anagram/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/valid-anagram/submissions/">242. Valid Anagram</a>
