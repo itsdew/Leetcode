b5f35761672de897a926d1d6c363d22c2c7ffd4c
Devendra Uraon <devendra123world@gmail.com>
2022-08-25 22:55:14 +0530

    Add files via upload

---
+++ 383. Ransom Note/326. Power of Three.cpp
@@ -0,0 +1,25 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+    bool a = 0;
+	int alphabet[26]={0};
+    int alphabet1[26]={0};
+	for(int i = 0; i < ransomNote.size(); i++){
+		alphabet[ransomNote[i]-'a']++;
+	}
+        for(int i = 0; i < magazine.size(); i++){
+		alphabet1[magazine[i]-'a']++;
+	}
+        for(int i = 0; i < ransomNote.size(); i++){
+            if(alphabet[ransomNote[i]-'a'] <= alphabet1[ransomNote[i]-'a']){
+                a = 1;
+            }
+            else{
+                a = 0;
+                break;
+            }
+    }
+        return a;
+    }
+};
---
+++ 383. Ransom Note/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/ransom-note/">383. Ransom Note</a>
