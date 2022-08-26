c56b73ed7761f80306fed2d6007c1c6feaf298b7
Devendra Uraon <devendra123world@gmail.com>
2022-08-26 23:37:09 +0530

    Add files via upload

---
+++ 349. Intersection of Two Arrays/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,23 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+
+     unordered_set<int> s;
+        for(int i=0; i<a.size(); i++)
+           s.insert(a[i]);
+        
+      vector<int> res;
+        
+        for(int j=0; j<b.size(); j++)
+        {
+            if(s.find(b[j])!=s.end())
+            {
+                res.push_back(b[j]);
+                s.erase(b[j]);
+            }
+        }
+        return res;
+    }
+    
+};
---
+++ 349. Intersection of Two Arrays/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/intersection-of-two-arrays/">349. Intersection of Two Arrays</a>
