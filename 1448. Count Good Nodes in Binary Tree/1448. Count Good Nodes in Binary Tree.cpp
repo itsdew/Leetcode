9b6eac452ee452e5a33f3b4514237059a8845492
Devendra Uraon <devendra123world@gmail.com>
2022-09-01 21:28:38 +0530

    Add files via upload

---
+++ 1448. Count Good Nodes in Binary Tree/1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,16 @@
+//Author: Devendra Uraon
+class Solution {
+    int sum = 0;
+    void doRec(TreeNode* root, int currentMax) {
+        if(root) {
+            if(root->val >= currentMax || currentMax == INT_MIN) sum++;
+            doRec(root->left, max(currentMax, root->val));
+            doRec(root->right, max(currentMax, root->val));
+        }
+    }
+public:
+    int goodNodes(TreeNode* root) {
+        doRec(root, INT_MIN);
+        return sum;
+    }
+};
---
+++ 1448. Count Good Nodes in Binary Tree/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/count-good-nodes-in-binary-tree/">1448. Count Good Nodes in Binary Tree</a>
