5a6715cfa923b74d8f49642099ac0b11421dfa8a
Devendra Uraon <devendra123world@gmail.com>
2022-09-05 22:36:26 +0530

    Add files via upload

---
+++ 429. N-ary Tree Level Order Traversal/429. N-ary Tree Level Order Traversal.cpp
@@ -0,0 +1,32 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> ans;
+        queue<vector<Node*>> q;
+        vector<int> temp;
+        
+        if(!root) return ans;
+        
+        temp.push_back(root -> val);
+        q.push(root -> children);
+        ans.push_back(temp);
+ 
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> temp;
+            while(size--){
+                auto cur = q.front();
+                q.pop();
+                
+                for(auto it:cur){
+                    q.push(it -> children);
+                    temp.push_back(it -> val);
+                }
+            }
+            if(temp.size() > 0)
+                ans.push_back(temp);
+        }
+        return ans;
+    }
+};
---
+++ 429. N-ary Tree Level Order Traversal/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/n-ary-tree-level-order-traversal/">429. N-ary Tree Level Order Traversal</a>
