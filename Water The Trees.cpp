e946c23977ead5901a28655d20b65255b410ad67
Devendra Uraon <devendra123world@gmail.com>
2022-09-05 23:09:04 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,8 +1,6 @@
-// 2022-05-24
+//Question: https://codeforces.com/contest/1661/problem/C
+//Author: Devendra Uraon
 #include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
@@ -10,41 +8,49 @@
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 1000000007
+#define MOD 998244353LL
 using namespace std;
 
 int main(){
-    fastio;
-	int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        vi vec(n);
-        for(int i{0}; i < n; ++i){
-            cin >> vec[i];
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int n; cin >> n;
+    vi C(n);
+    for(int i = 0; i < n; ++i){
+        cin >> C[i];
+    }
+    vector<string> vec(n);
+    for(int i = 0; i < n; ++i){
+        cin >> vec[i];
+    }
+    vector<vl> dp(n, vl(2, MAX));
+    dp[0][0] = 0;
+    dp[0][1] = C[0];
+    for(int i{1}; i < n; ++i){
+        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
+            cout << -1;
+            return 0;
         }
-        if(n & 1){
-            cout << "NO\n";
-            continue;
+        string p = vec[i - 1];
+        string pr = p;
+        reverse(pr.begin(), pr.end());
+        string r = vec[i];
+        reverse(r.begin(), r.end());
+        if(vec[i] >= p){
+            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
         }
-        vi v;
-        sort(vec.begin(), vec.end());
-        for(int i{0}; i < n / 2; ++i){
-            v.push_back(vec[i]);
-            v.push_back(vec[i + n / 2]);
+        if(vec[i] >= pr){
+            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
         }
-        bool flag = true;
-        for(int i{0}; i < n; ++i){
-            if(v[i] > v[(i + 1) % n] && v[i] > v[(i + n - 1) % n]) continue;
-            if(v[i] < v[(i + 1) % n] && v[i] < v[(i + n - 1) % n]) continue;
-            flag = false;
+        if(r >= p){
+            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
         }
-        if(flag){
-            cout << "YES\n";
-            for(auto& i : v) cout << i << " ";
-            cout << "\n";
+        if(r >= pr){
+            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
         }
-        else cout << "NO\n";
+    }
+    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
+    if(ans == MAX) cout << -1;
+    else{
+        cout << ans;
     }
 }
