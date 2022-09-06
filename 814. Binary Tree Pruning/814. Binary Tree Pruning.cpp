9ae27631c4bf3c1a46b467147adfd37bc7bd9e3a
Devendra Uraon <devendra123world@gmail.com>
2022-09-06 21:43:56 +0530

    Add files via upload

---
+++ 814. Binary Tree Pruning/814. Binary Tree Pruning.cpp
@@ -0,0 +1,11 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root) return NULL;
+        root->left = pruneTree(root -> left);
+        root->right = pruneTree(root -> right);
+        if(root->val == 0 &&   !root -> left && !root -> right) return NULL;
+        return root;
+    }
+};
---
+++ 814. Binary Tree Pruning/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/binary-tree-pruning/">814. Binary Tree Pruning</a>
