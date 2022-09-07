d9f1d6d293d9eaffc2a6eaa47361f69838edb82b
Devendra Uraon <devendra123world@gmail.com>
2022-09-07 22:53:18 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,6 +1,8 @@
-//Question: https://codeforces.com/contest/1661/problem/C
-//Author: Devendra Uraon
+// 2022-05-04
 #include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
@@ -8,49 +10,71 @@
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 998244353LL
+#define MOD 1000000007
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n; cin >> n;
-    vi C(n);
-    for(int i = 0; i < n; ++i){
-        cin >> C[i];
+vi graph[5000];
+vi graph2[5000];
+int n;
+int ch[4]{};
+int ans{0};
+
+void dfs(int L, vi& vec, int k){
+    if(L == 4){        
+        ans++;
     }
-    vector<string> vec(n);
-    for(int i = 0; i < n; ++i){
-        cin >> vec[i];
-    }
-    vector<vl> dp(n, vl(2, MAX));
-    dp[0][0] = 0;
-    dp[0][1] = C[0];
-    for(int i{1}; i < n; ++i){
-        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
-            cout << -1;
-            return 0;
-        }
-        string p = vec[i - 1];
-        string pr = p;
-        reverse(pr.begin(), pr.end());
-        string r = vec[i];
-        reverse(r.begin(), r.end());
-        if(vec[i] >= p){
-            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
-        }
-        if(vec[i] >= pr){
-            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
-        }
-        if(r >= p){
-            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
-        }
-        if(r >= pr){
-            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
-        }
-    }
-    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
-    if(ans == MAX) cout << -1;
     else{
-        cout << ans;
+        if(L == 0 || L == 1){
+            for(int i{k + 1}; i < n; ++i){
+                ch[L] = i;
+                dfs(L + 1, vec, i);
+            }
+        }
+        else if(L == 2){
+            for(auto& i : graph[ch[0]]){
+                if(i > ch[1]){
+                    ch[2] = i;
+                    dfs(L + 1, vec, i);
+                }
+            }
+        }
+        else if(L == 3){
+            for(auto& i : graph2[ch[1]]){
+                if(i > ch[2]){
+                    ch[3] = i;
+                    dfs(L + 1, vec, i);
+                }
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    fastio;
+	int t;
+    cin >> t;
+    while(t--){
+        ans = 0;
+        cin >> n;
+        vi vec(n);
+        for(int i{0}; i < n; ++i){
+            cin >> vec[i];
+            graph[i].clear();
+            graph2[i].clear();
+        }
+        for(int i{0}; i < n; ++i){
+            for(int j{i + 1}; j < n; ++j){
+                if(vec[i] < vec[j]){
+                    graph[i].push_back(j);
+                }
+                if(vec[i] > vec[j]){
+                    graph2[i].push_back(j);
+                }
+            }
+        }
+        dfs(0, vec, -1);
+        cout << ans << "\n";
     }
 }
