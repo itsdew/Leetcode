6b5f2dfdb91098f781793419182ab1ca0a85008f
Devendra Uraon <devendra123world@gmail.com>
2022-09-07 22:23:05 +0530

    Add files via upload

---
+++ 39. Combination Sum/39. Combination Sum.cpp
@@ -0,0 +1,22 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        ans_fun(0, temp, ans, candidates, target, 0);
+        return ans;
+    }
+    
+    void ans_fun(int sums, vector<int>&temp, vector<vector<int>>&ans, vector<int> &cand, int targ, int pos){
+        if(sums == targ){
+            ans.push_back(temp);
+        }
+        if(sums < targ && pos < cand.size()){
+            temp.push_back(cand[pos]);
+            ans_fun(sums + cand[pos], temp, ans, cand, targ, pos);
+            temp.pop_back();
+            ans_fun(sums, temp, ans, cand, targ, pos+1);
+        }
+    }
+};
---
+++ 39. Combination Sum/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/combination-sum/">39. Combination Sum</a>
