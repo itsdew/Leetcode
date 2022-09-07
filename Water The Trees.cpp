918c6b9290ff2967daf166c35b68188de8687d8b
Devendra Uraon <devendra123world@gmail.com>
2022-09-08 01:31:50 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,8 +1,6 @@
-// 2022-05-04
+//Question: https://codeforces.com/contest/1661/problem/C
+//Author: Devendra Uraon
 #include <bits/stdc++.h>
-#define fastio                    \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
@@ -10,71 +8,49 @@
 #define vp vector<pi>
 #define ll long long
 #define MAX 2147000000
-#define MOD 1000000007
+#define MOD 998244353LL
 using namespace std;
 
-vi graph[100001];
-
 int main(){
-    fastio;
-	int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        vi A(n), B(n);
-        for(int i{0}; i < n; ++i){
-            cin >> A[i];
-        }        
-        for(int i{1}; i <= n; ++i) graph[i].clear();
-        for(int i{0}; i < n; ++i){
-            cin >> B[i];
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int n; cin >> n;
+    vi C(n);
+    for(int i = 0; i < n; ++i){
+        cin >> C[i];
+    }
+    vector<string> vec(n);
+    for(int i = 0; i < n; ++i){
+        cin >> vec[i];
+    }
+    vector<vl> dp(n, vl(2, MAX));
+    dp[0][0] = 0;
+    dp[0][1] = C[0];
+    for(int i{1}; i < n; ++i){
+        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
+            cout << -1;
+            return 0;
         }
-        ll ans{0};
-        for(int i{0}; i < n; ++i){
-            graph[A[i]].push_back(B[i]);
-            graph[B[i]].push_back(A[i]);
+        string p = vec[i - 1];
+        string pr = p;
+        reverse(pr.begin(), pr.end());
+        string r = vec[i];
+        reverse(r.begin(), r.end());
+        if(vec[i] >= p){
+            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
         }
-        vi ch(n + 1);
-        int l{1}, r{n};
-        vi select(n + 1);
-        bool flag = true;
-        for(int i{1}; i <= n; ++i){
-            if(ch[i] == 0){
-                queue<int> Q;
-                Q.push(i);
-                ch[i] = 1;
-                vi vec;
-                while(!Q.empty()){
-                    int x{Q.front()};
-                    Q.pop();
-                    vec.push_back(x);
-                    for(auto& k : graph[x]){
-                        if(ch[k] == 0){
-                            ch[k] = 1;
-                            Q.push(k);
-                        }
-                    }
-                }
-                if(vec.size() == 1) continue;
-                for(int j{0}; j < (int)vec.size(); ++j){
-                    if(flag){
-                        select[vec[j]] = l;
-                        l++;
-                        flag = false;
-                    }
-                    else{
-                        select[vec[j]] = r;
-                        r--;
-                        flag = true;
-                    }
-                }
-                for(int j{0}; j < (int)vec.size() - 1; ++j){
-                    ans += abs(select[vec[j + 1]] - select[vec[j]]);
-                }
-                ans += abs(select[vec[0]] - select[vec.back()]);                
-            }
+        if(vec[i] >= pr){
+            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
         }
-        cout << ans << "\n";
+        if(r >= p){
+            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
+        }
+        if(r >= pr){
+            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
+        }
+    }
+    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
+    if(ans == MAX) cout << -1;
+    else{
+        cout << ans;
     }
 }
