eded6d22f8f7d4e579fe9295086e5ec327c7ab06
Devendra Uraon <devendra123world@gmail.com>
2022-09-09 22:20:56 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,4 +1,4 @@
-// 2022-03-30
+// 2022-03-14
 #include <bits/stdc++.h>
 #define fastio                    \
 	ios_base::sync_with_stdio(0); \
@@ -13,70 +13,39 @@
 #define MOD 998244353LL
 using namespace std;
 
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
 int main(){
     fastio;
-	int n;
+    int n;
     cin >> n;
-    vector<vi> graph(n + 1);
-    for(int i{0}; i < n - 1; ++i){
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
-    vi ch(n + 1);
-    ch[1] = 1;
-    vi seq(n);
+    vi vec(n);
     for(int i{0}; i < n; ++i){
-        cin >> seq[i];
+        cin >> vec[i];
     }
-    if(seq.front() != 1){
-        cout << "No";
-        return 0;
+    vi post(n);
+    post[n - 1] = vec[n - 1];
+    for(int i{n - 2}; i >= 0; --i){
+        post[i] = gcd(vec[i], post[i + 1]);
     }
-    queue<int> Q;
-    Q.push(1);
-    vi par(n + 1);
-    par[1] = -1;
-    while(!Q.empty()){
-        int x{Q.front()};
-        Q.pop();
-        for(auto& i : graph[x]){
-            if(ch[i] == 0){
-                ch[i] = 1;
-                par[i] = x;
-                Q.push(i);
-            }
+    vi pre(n);
+    post[0] = vec[0];
+    for(int i{1}; i < n; ++i){
+        pre[i] = gcd(vec[i], pre[i - 1]);
+    }
+    int ans{0};
+    for(int i{0}; i < n; ++i){
+        if(i == 0){
+            ans = max(ans, post[1]);
+        }
+        else if(i == n -1){
+            ans = max(ans, pre[n - 2]);
+        }
+        else{
+            ans = max(ans, gcd(pre[i - 1], post[i + 1]));
         }
     }
-    for(int i{0}; i < n; ++i) ch[i] = 0;
-    ch[1] = 1;
-    Q.push(1);
-    bool flag = true;
-    int cur{0};
-    while(!Q.empty()){
-        int x{Q.front()};
-        Q.pop();
-        vi vec;
-        sort(graph[x].begin(), graph[x].end());
-        int k = (int)graph[x].size();
-        if(x != 1) k--;
-        for(int i{cur + 1}; i < cur + 1 + k && i < n; ++i){
-            vec.push_back(seq[i]);
-            Q.push(seq[i]);
-        }
-        if(x != 1) vec.push_back(par[x]);
-        cur += k;
-        sort(vec.begin(), vec.end());
-        for(auto& i : vec) cout << i << " ";
-        cout << "\n";
-        for(auto& i : graph[x]) cout << i << " ";
-        cout << "\n";
-        if(vec != graph[x]){
-            flag = false;
-            break;
-        }
-    }
-    if(flag) cout << "Yes";
-    else cout << "No";
+    cout << ans;
 }
