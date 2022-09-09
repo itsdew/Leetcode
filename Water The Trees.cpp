9022d321713338a5c94d9e2b89fcb2ba0315432d
Devendra Uraon <devendra123world@gmail.com>
2022-09-09 21:54:23 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,100 +1,58 @@
-#pragma G++ optimize("Ofast")
-#pragma G++ optimize("unroll-loops")
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<cstring>
-#include<functional>
-#include<queue>
-#include<unordered_map>
-#include<map>
-#include<set>
-#include<stack>
-#include<cmath>
-#include<bitset>
-#include<iomanip>
-#include<numeric>
-
+// 2022-03-30
+#include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define MAX 2147000000
+#define MOD 998244353LL
 using namespace std;
 
-using lli = long long;
-using pii = pair<lli, lli>;
-const int INF = 1e9;
-const lli inf = 1e18;
-const int maxn = 1e5+5;
-int n, m, ans, mx;
-int sum[maxn];
-
-struct Node {
-	int a, b, c;
-	bool operator < (const Node x) const {
-		return c<x.c;
-	}
-} a[maxn];
-
-lli exgcd(lli a, lli b, lli &x, lli &y) {
-	if(!b) {
-		x = 1;
-		y = 0;
-		return a;
-	}
-	
-	lli d = exgcd(b, a%b, y, x);
-	y = y - a/b*x;
-	
-	return d;
-}
-//gcd = exgcd(a, b, x, y)
-//x = x*c/gcd;
-//y = y*c/gcd;
-
-
-void solve() {
-	cin>>n;
-	ans = 0;
-	
-	for(int i=1; i<=n; ++i) {
-		cin>>a[i].a>>a[i].b;
-		a[i].c = a[i].b-a[i].a;
-		sum[n]+=a[i].a;
-	}
-	sort(a+1, a+1+n);
-	
-	mx = n;
-	for(int i=n-1; i>=0; --i) {
-		sum[i] = sum[i+1]+a[i+1].c;
-		if(a[i+1].c>0)	mx = i;
-	}
-	
-//	for(int i=0; i<=n; ++i) {
-//		cout<<sum[i]<<' ';
-//	}cout<<'\n';
-	
-	cin>>m;
-	
-	for(int i=1; i<=m; ++i) {
-		lli a, b, x, y;
-		cin>>a>>b;
-		lli gcd = exgcd(a, b, x, y);
-		if(n%gcd) {
-			cout<<"-1\n";
-			continue;
-		}
-		x = x*n/gcd;
-//		cout<<x<<' ';
-		x+=ceil((1.0*mx/a-x)/(b/gcd))*(b/gcd);
-		if(a*x<mx)	x+=b/gcd;
-		y = x-b/gcd;
-//		cout<<x<<' '<<y<<' ';
-		cout<<max((0<=a*x&&a*x<=n) ? sum[a*x] : -1, (0<=a*y&&a*y<=n) ? sum[a*y] : -1)<<'\n';
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	int t=1;
-//	cin>>t;
-	while(t--)solve();
-	return 0;
+int main(){
+    fastio;
+	int n;
+    cin >> n;
+    vector<vi> graph(n + 1);
+    for(int i{0}; i < n - 1; ++i){
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    vi ch(n + 1);
+    ch[1] = 1;
+    vi seq(n);
+    for(int i{0}; i < n; ++i){
+        cin >> seq[i];
+    }
+    if(seq.front() != 1){
+        cout << "No";
+        return 0;
+    }
+    queue<int> Q;
+    Q.push(1);
+    bool flag = true;
+    int cur{0};
+    while(!Q.empty()){
+        int x{Q.front()};
+        Q.pop();
+        vi vec;
+        sort(graph[x].begin(), graph[x].end());
+        for(int i{cur + 1}; i < cur + 1 + (int)graph[x].size(); ++i){
+            vec.push_back(seq[i]);
+        }
+        cur += (int)graph[x].size();
+        sort(vec.begin(), vec.end());
+        if(vec != graph[x]){
+            flag = false;
+            break;
+        }
+    }
+    if(flag) cout << "Yes";
+    else cout << "No";
 }
