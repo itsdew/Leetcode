78be958461917289293c0a189e994ed639475b6a
Devendra Uraon <devendra123world@gmail.com>
2022-09-10 22:51:01 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -17,17 +17,26 @@ int main(){
     fastio;
 	int n; ll s;
     cin >> n >> s;
-    vl pre(n + 1);
+    vp vec(n);
+    for(int i{0}; i < n; ++i){
+        cin >> vec[i].first;
+        vec[i].second = i;
+    }
+    sort(vec.begin(), vec.end());
+    vl A(n + 1);
+    vl B(n + 1);
     for(int i{1}; i <= n; ++i){
-        cin >> pre[i];
-        pre[i] += pre[i - 1];
+        A[i] = vec[i - 1].first;
+        A[i] += A[i - 1];
+        B[i] = vec[i - 1].second + 1;
+        B[i] += B[i - 1];  
     }
     ll mx{0};
     ll cost{0};
     for(ll i{1}; i <= n; ++i){
-        if(pre[i] + i * (i + 1) / 2 * i <= s){
+        if(A[i] + B[i] * i <= s){
             mx = i;
-            cost = pre[i] + i * (i + 1) / 2 * i;
+            cost = A[i] + B[i] * i;
         }
     }
     cout << mx << " " << cost;
