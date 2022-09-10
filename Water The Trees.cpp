0fed7b694432d8c0ceca01669d65d368205487d6
Devendra Uraon <devendra123world@gmail.com>
2022-09-10 22:47:43 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,6 +1,8 @@
-//Question: https://codeforces.com/contest/1661/problem/C
-//Author: Devendra Uraon
+// 2022-04-07
 #include <bits/stdc++.h>
+#define fastio                    \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0);
 #define vi vector<int>
 #define vl vector<long long>
 #define vc vector<char>
@@ -12,45 +14,21 @@
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n; cin >> n;
-    vi C(n);
-    for(int i = 0; i < n; ++i){
-        cin >> C[i];
+    fastio;
+	int n; ll s;
+    cin >> n >> s;
+    vl pre(n + 1);
+    for(int i{1}; i <= n; ++i){
+        cin >> pre[i];
+        pre[i] += pre[i - 1];
     }
-    vector<string> vec(n);
-    for(int i = 0; i < n; ++i){
-        cin >> vec[i];
-    }
-    vector<vl> dp(n, vl(2, MAX));
-    dp[0][0] = 0;
-    dp[0][1] = C[0];
-    for(int i{1}; i < n; ++i){
-        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
-            cout << -1;
-            return 0;
-        }
-        string p = vec[i - 1];
-        string pr = p;
-        reverse(pr.begin(), pr.end());
-        string r = vec[i];
-        reverse(r.begin(), r.end());
-        if(vec[i] >= p){
-            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
-        }
-        if(vec[i] >= pr){
-            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
-        }
-        if(r >= p){
-            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
-        }
-        if(r >= pr){
-            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
+    ll mx{0};
+    ll cost{0};
+    for(ll i{1}; i <= n; ++i){
+        if(pre[i] + i * (i + 1) / 2 * i <= s){
+            mx = i;
+            cost = pre[i] + i * (i + 1) / 2 * i;
         }
     }
-    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
-    if(ans == MAX) cout << -1;
-    else{
-        cout << ans;
-    }
+    cout << mx << " " << cost;
 }
