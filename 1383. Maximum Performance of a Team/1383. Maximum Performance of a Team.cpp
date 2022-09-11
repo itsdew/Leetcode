e22ac27d0a5a0e59e3a5edc9cad80b2016dbc66a
Devendra Uraon <devendra123world@gmail.com>
2022-09-11 23:06:05 +0530

    Add files via upload

---
+++ 1383. Maximum Performance of a Team/1383. Maximum Performance of a Team.cpp
@@ -0,0 +1,23 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int maxPerformance(int n, std::vector<int> &speed, vector<int> &efficiency, int k){
+        vector<pair<int, int>> vec;
+        long sum = 0, result = 0; 
+        for (int i = 0; i < n; i++) vec.push_back({efficiency[i], speed[i]});
+
+        sort(vec.rbegin(), vec.rend());
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < n; i++) {
+            if (i >= k) {
+                sum -= pq.top();
+                pq.pop();
+            }
+            sum += vec[i].second;
+            pq.push(vec[i].second);
+            result = max(result, sum * vec[i].first);
+        }
+        return result % static_cast<int>(1e9 + 7);
+    }
+};
---
+++ 1383. Maximum Performance of a Team/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/maximum-performance-of-a-team/">1383. Maximum Performance of a Team</a>
