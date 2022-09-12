3292549643edb1f5b601c8a3d7838a370b2d463e
Devendra Uraon <devendra123world@gmail.com>
2022-09-12 22:31:14 +0530

    Add files via upload

---
+++ 948. Bag of Tokens/948. Bag of Tokens.cpp
@@ -0,0 +1,25 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int c = 0;
+        sort(tokens.begin(),tokens.end());
+        int l = 0, r = tokens.size()-1;
+        int ans = 0;
+        while(l <= r){
+            if(power >= tokens[l]){
+                c++;
+                ans = max(ans,c);
+                power -= tokens[l];
+                l++;
+            }
+            else if(power < tokens[l] && c>=1){
+                power += tokens[r];
+                c--; r--;
+            }
+            else if(c==0)
+                break;
+        }
+        return ans;
+    }
+};
---
+++ 948. Bag of Tokens/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/bag-of-tokens/">948. Bag of Tokens</a>
