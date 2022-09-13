a7a2d16d8c922e0ba17867a20ef39e23fbc0ca56
Devendra Uraon <devendra123world@gmail.com>
2022-09-13 21:38:12 +0530

    Add files via upload

---
+++ 393. UTF-8 Validation/393. UTF-8 Validation.cpp
@@ -0,0 +1,20 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int m[6] = {128, 192, 224, 240, 248, 10000000};
+        int nbits,remaningbits = 0;
+        for(auto d: data){
+            nbits = 0;
+            while(d >= m[nbits]) nbits++;
+            if (nbits >= 5) return false;
+            if (remaningbits>0){
+                if (nbits!=1) return false;
+                remaningbits--;
+            }
+            else if (nbits >= 2) remaningbits = nbits-1;
+            else if (nbits == 1) return false;
+        }
+        return remaningbits == 0;
+    }
+};
---
+++ 393. UTF-8 Validation/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/utf-8-validation/">393. UTF-8 Validation</a>
