f5bbc5f424b7d5be6eeda54ab756ed0868e7d7a5
Devendra Uraon <devendra123world@gmail.com>
2022-09-15 00:27:04 +0530

    Add files via upload

---
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree/1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -0,0 +1,24 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> v,int &count){
+        if (!root->left && !root->right){ // at a leaf node 
+            v[root->val-1] = v[root->val -1] +1;
+            int odd = 0;
+            for(int i:v){
+                if (i%2 == 1) odd++;
+            }
+            if (odd <=1 ) count++;
+            return;
+        }
+        v[root -> val-1] = v[root -> val -1] +1;
+        if (root -> left) solve(root -> left, v,count);
+        if (root -> right) solve(root -> right, v,count);
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> v(9, 0);
+        int count = 0;
+        solve(root, v,count);
+        return count;
+    }
+};
---
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/">1457. Pseudo-Palindromic Paths in a Binary Tree</a>
