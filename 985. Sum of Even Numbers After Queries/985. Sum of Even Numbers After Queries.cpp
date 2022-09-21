44d1c5e646e17922afa3abe7406718123a37ee99
Devendra Uraon <devendra123world@gmail.com>
2022-09-21 11:58:29 +0530

    Add files via upload

---
+++ 985. Sum of Even Numbers After Queries/985. Sum of Even Numbers After Queries.cpp
@@ -0,0 +1,18 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& n, vector<vector<int>>& q) {
+        int sum = 0, val, index;
+        vector<int> vec;
+        for(int i = 0; i < n.size(); i++) if(n[i]%2 == 0) sum += n[i];
+        for(int i = 0; i < q.size(); i++){
+            val = q[i][0];
+            index = q[i][1];
+            if(n[index]%2 == 0) sum -= n[index];
+            n[index] += val;
+            if(n[index]%2 == 0) sum += n[index];
+            vec.push_back(sum);
+        } 
+        return vec;
+    }
+};
---
+++ 985. Sum of Even Numbers After Queries/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/sum-of-even-numbers-after-queries/">985. Sum of Even Numbers After Queries</a>
