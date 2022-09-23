67a4b997fdb23f5c085baae8fe318c5b6030ce18
Devendra Uraon <devendra123world@gmail.com>
2022-09-23 23:40:15 +0530

    Time: 104 ms (60.87%), Space: 6 MB (22.83%)

--- 1680. Concatenation of Consecutive Binary Numbers/1680. Concatenation of Consecutive Binary Numbers.cpp
+++ 1680. Concatenation of Consecutive Binary Numbers/1680. Concatenation of Consecutive Binary Numbers.cpp
@@ -1,13 +1,13 @@
-//Author: Devendra Uraon
+//Author: Devendra Uraon 
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        long long int val=0;
-        int i=1;
-        while(i<=n)
+        long long int val = 0;
+        int i = 1;
+        while(i <= n)
         {
-            val=((val << (1 + int(log2(i)))) % 1000000007 +i) % 1000000007;
-            i ++ ;
+            val = ((val << (1 + int(log2(i)))) % 1000000007 +i) % 1000000007;
+            i++ ;
         }
         return val;
     }
