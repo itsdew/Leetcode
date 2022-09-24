fea6b5dd3403771f59190ee81d5a33c1fc0288aa
Devendra Uraon <devendra123world@gmail.com>
2022-09-24 21:53:41 +0530

    Add files via upload

---
+++ 113. Path Sum II/113. Path Sum II.cpp
@@ -0,0 +1,25 @@
+//Author: Devendra Uraon
+class Solution {  
+    vector<vector<int>> ans;
+    vector<int> path;
+    int sum = 0, target;
+    
+    void dfs(TreeNode* x) {
+        if (!x) return;
+        sum += x->val;
+        path.push_back(x -> val);
+        if (sum == target && !x -> left && !x -> right)
+            ans.push_back(path);
+        dfs(x -> left);
+        dfs(x -> right);
+        sum -= x -> val;
+        path.pop_back();
+    }
+    
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        target = targetSum;
+        dfs(root);
+        return ans;
+    }
+};
---
+++ 113. Path Sum II/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/path-sum-ii/">113. Path Sum II</a>
