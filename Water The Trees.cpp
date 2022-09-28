48fb7ec0bae1b91fb6f20db32d0d612735b00afd
Devendra Uraon <devendra123world@gmail.com>
2022-09-28 21:50:48 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,56 +1,75 @@
-//Question: https://codeforces.com/contest/1661/problem/C
-//Author: Devendra Uraon
+// Snippets: https://github.com/prabhavdogra/CPSnippets
 #include <bits/stdc++.h>
-#define vi vector<int>
-#define vl vector<long long>
-#define vc vector<char>
-#define pi pair<int, int>
-#define vp vector<pi>
-#define ll long long
-#define MAX 2147000000
-#define MOD 998244353LL
-using namespace std;
+using namespace std; 
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+#ifdef prabhav_
+    // #define TERMINAL
+    #include "Headers/debug.cpp"
+#else
+    #define d(...) 0
+    #pragma GCC optimize("O3")
+    #pragma GCC target("avx,avx2,sse,sse2,sse3,sse4,popcnt,fma")
+#endif
 
-int main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n; cin >> n;
-    vi C(n);
-    for(int i = 0; i < n; ++i){
-        cin >> C[i];
+#define int long long int
+#define ld long double
+#define forp(i, a, b) for (int i = (int)a; i < (int)b; i++)
+#define sz(a) (int)a.size()
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define uniq(a) (a).erase(unique(all(a)), (a).end())
+#define fix(f, n) fixed << setprecision(n) << f
+#define setbits(x) __builtin_popcount(x)
+#define setbitsll(x) __builtin_popcountll(x)
+#define MSB(n) (31 - __builtin_clz(n))
+constexpr int64_t INF = 2e18;
+constexpr int64_t M = 1 ? 1e9 + 7 : 998'244'353;
+int ceil(int a, int b) { return (a + b - 1) / b; }
+
+
+void prabhav() {
+    int n, ans = 1;
+    string s;
+    cin >> n >> s;
+    n *= 2;
+    auto rec = [&] (const auto &self, int start, int end) {
+        if(start > end) return;
+        ans++;
+        int cstart = start, cnt = 0;
+        for(int i = start; i < end + 1; i++) {
+            if(s[i] == '(') cnt++;
+            else cnt--;
+            if(cnt == 0) {
+                self(self, cstart + 1, i - 1);
+                cstart = i + 1;
+            }
+        }
+    };
+    int cnt = 0, start = 0;
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '(') cnt++;
+        else cnt--;
+        if(cnt == 0) {
+            rec(rec, start + 1, i - 1);
+            start = i + 1;
+        }
     }
-    vector<string> vec(n);
-    for(int i = 0; i < n; ++i){
-        cin >> vec[i];
+    cout << ans << '\n';
+}
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        prabhav();
     }
-    vector<vl> dp(n, vl(2, MAX));
-    dp[0][0] = 0;
-    dp[0][1] = C[0];
-    for(int i{1}; i < n; ++i){
-        if(dp[i - 1][0] == MAX && dp[i - 1][1] == MAX){
-            cout << -1;
-            return 0;
-        }
-        string p = vec[i - 1];
-        string pr = p;
-        reverse(pr.begin(), pr.end());
-        string r = vec[i];
-        reverse(r.begin(), r.end());
-        if(vec[i] >= p){
-            dp[i][0] = min(dp[i][0], dp[i - 1][0]);
-        }
-        if(vec[i] >= pr){
-            dp[i][0] = min(dp[i][0], dp[i - 1][1]);
-        }
-        if(r >= p){
-            dp[i][1] = min(dp[i][1], dp[i - 1][0] + C[i]);
-        }
-        if(r >= pr){
-            dp[i][1] = min(dp[i][1], dp[i - 1][1] + C[i]);
-        }
-    }
-    ll ans = min(dp[n - 1][0], dp[n - 1][1]);
-    if(ans == MAX) cout << -1;
-    else{
-        cout << ans;
-    }
+    // cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
 }
