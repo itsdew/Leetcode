820a3ed3b0ed00a8cc52b30184308754eb43a7b1
Devendra Uraon <devendra123world@gmail.com>
2022-09-29 23:51:35 +0530

    Add files via upload

---
+++ 658. Find K Closest Elements/658. Find K Closest Elements.cpp
@@ -0,0 +1,15 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>, vector<pair<int,int>>> pq;
+        for(auto n: arr){
+            pq.push({abs(n - x), n});
+            if(pq.size() > k) pq.pop();
+        }
+        vector<int> ans;
+        while(!pq.empty()) ans.push_back(pq.top().second), pq.pop();
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
---
+++ 658. Find K Closest Elements/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/find-k-closest-elements/">658. Find K Closest Elements</a>
