70b91583224d9a1848bd65252a6fa8b45479313f
Devendra Uraon <devendra123world@gmail.com>
2022-10-01 21:50:10 +0530

    Add files via upload

---
+++ 91. Decode Ways/91. Decode Ways.cpp
@@ -0,0 +1,23 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int numDecodings(string s){
+        vector<int> hg(s.size()+1, 0);
+        int ans = 0;
+        ans = helper(s, 0, hg);
+        return ans;
+    }
+    int helper(string s, int i, vector<int>&hs) {
+        if(s.size() == i) return 1;
+        if(s[i] == '0') return 0;
+        if(hs[i] > 0) return hs[i];
+        if(i == s.size()-1) return 1;
+        if(i < s.size()-1){
+            int num = (s[i] - '0') * 10 + (s[i+1] - '0');
+            if(num>0 && num<=26){
+                return hs[i] = helper(s, i+1, hs) + helper(s, i+2, hs);
+            }
+        }
+        return hs[i]=helper(s, i+1, hs);
+    }
+};
---
+++ 91. Decode Ways/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/decode-ways/">91. Decode Ways</a>
