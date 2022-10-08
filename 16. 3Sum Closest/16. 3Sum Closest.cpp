ab8e58e9f13ff72320cc85bc93241f9b2830defe
Devendra Uraon <devendra123world@gmail.com>
2022-10-08 23:22:54 +0530

    Add files via upload

---
+++ 16. 3Sum Closest/16. 3Sum Closest.cpp
@@ -0,0 +1,23 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), sum = nums[0] + nums[1] + nums[2];
+        int closest = abs(target - sum);
+        for(int first = 0; first < n-2; first++){
+            int second = first + 1, third = n -1;
+            while(second < third){
+                int sum2 = nums[first] + nums[second]+ nums[third];
+                int checking = abs(target - sum2);
+                if(closest > checking){
+                    sum = sum2;
+                    closest = checking;
+                }
+                if(sum2 > target) third--;
+                else second++;
+            }
+        }
+        return sum;
+    }
+};
---
+++ 16. 3Sum Closest/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/3sum-closest/">16. 3Sum Closest</a>
