2c6c29cd1fb2b5f2468ee82141dccea38ac14dad
Devendra Uraon <devendra123world@gmail.com>
2022-10-11 23:18:04 +0530

    Add files via upload

---
+++ 334. Increasing Triplet Subsequence/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,14 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size() < 3) return false;
+        int first = INT_MAX, second = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > second) return true;
+            else if(nums[i]>first && nums[i]<second) second = nums[i];
+            else if(nums[i] < first) first = nums[i];
+        }
+        return false;
+    }
+};
---
+++ 334. Increasing Triplet Subsequence/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/increasing-triplet-subsequence/">334. Increasing Triplet Subsequence</a>
