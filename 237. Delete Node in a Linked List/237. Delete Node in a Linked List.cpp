66f057f5a7c67eaa3e9563b0f85a222801c27c24
Devendra Uraon <devendra123world@gmail.com>
2022-10-13 23:14:03 +0530

    Add files via upload

---
+++ 237. Delete Node in a Linked List/237. Delete Node in a Linked List.cpp
@@ -0,0 +1,8 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node -> val = node -> next -> val;
+        node -> next = node -> next -> next;
+    }
+};
