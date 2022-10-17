6fea7ea040745efdaa3421eef74f09d761efdb5c
Devendra Uraon <devendra123world@gmail.com>
2022-10-17 11:47:57 +0530

    Add files via upload

---
+++ 1832. Check if the Sentence Is Pangram/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,10 @@
+//Author: Devendra Uraon
+class Solution {
+	public:
+	bool checkIfPangram(string sentence) {
+		unordered_map<char , int> mp;
+		for( int i = 0; i < sentence.size(); i++) mp[sentence[i]]++;
+		if(mp.size() == 26) return true;
+		return false;
+	}
+};
---
+++ 1832. Check if the Sentence Is Pangram/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/check-if-the-sentence-is-pangram/">1832. Check if the Sentence Is Pangram</a>
