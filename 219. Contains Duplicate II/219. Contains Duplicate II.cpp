570d1ca505510082ae8cd23ea705e72559bfbd69
Devendra Uraon <devendra123world@gmail.com>
2022-10-21 23:48:28 +0530

    Add files via upload

---
+++ 219. Contains Duplicate II/219. Contains Duplicate II.cpp
@@ -0,0 +1,15 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        size_t size = nums.size();
+        int i;
+        unordered_map<int,int> m;
+        for (i = 0; i < size; ++i) {
+            if (m.find(nums[i]) == m.end()) m[nums[i]] = i;
+            else if (i-m[nums[i]] <= k) return true; 
+            else m[nums[i]] = i;
+        }
+        return false;
+    }
+};
---
+++ 219. Contains Duplicate II/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/contains-duplicate-ii/">219. Contains Duplicate II</a>
