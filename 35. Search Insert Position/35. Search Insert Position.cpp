1b5f76d29234ea4bf0935eed8e79df2f851ffc59
Devendra Uraon <devendra123world@gmail.com>
2022-10-31 00:15:43 +0530

    Space Complexity
    Better Space Complexity by Taniia

--- 35. Search Insert Position/35. Search Insert Position.cpp
+++ 35. Search Insert Position/35. Search Insert Position.cpp
@@ -5,11 +5,7 @@ public:
         int low = 0, high = nums.size()-1;
         while (low <= high) {
             int mid = low + (high-low)/2;
-
-            if (nums[mid] < target)
-                low = mid+1;
-            else
-                high = mid-1;
+            if ((nums[mid] < target) ? low = mid+1 : high = mid-1);
         }
         return low;
     }
