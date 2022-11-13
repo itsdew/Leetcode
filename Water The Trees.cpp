d12a9d5d7d7c2a3d50bf6bd88d2acf94d175c981
Devendra Uraon <devendra123world@gmail.com>
2022-11-13 13:34:46 +0530

    Update Water The Trees.cpp

--- Water The Trees.cpp
+++ Water The Trees.cpp
@@ -1,57 +1,75 @@
+//Question: https://codeforces.com/contest/1661/problem/C
+//Author: Devendra Uraon
 #include <bits/stdc++.h>
-
+#define vi vector<int>
+#define vl vector<long long>
+#define vc vector<char>
+#define pi pair<int, int>
+#define vp vector<pi>
+#define ll long long
+#define forloop for(ll i = 0; i < n; ++i)
+#define input cin >>
+#define print cout <<
+#define MAX 2147000000
+#define MOD 998244353LL
 using namespace std;
-using ll = long long;
-
-void solve();
-
-#define answer(out) { cout << (out) << '\n'; return; }
-#define answerExit(out) { cout << (out) << '\n'; exit(0); }
-
-int main() {
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int t = 1;
-    cin >> t;
-    do { solve(); } while (--t);
-    return 0;
-}
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> d1(n), d2(n);
-    for (int &x: d1) cin >> x;
-    for (int &x: d2) cin >> x;
-    map<int, int> c1, c2;
-    for (int x: d1) c1[x]++;
-    for (int x: d2) c2[x]++;
-    auto ok = [&](int d) -> bool {
-        vector<int> p;
-        auto b1 = c1, b2 = c2;
-        while (!b1.empty()) {
-            auto[x, cnt] = *--b1.end();
-            b1.erase(--b1.end());
-            int u = b2[x + d];
-            if (u > cnt) return false;
-            b2[x + d] -= u;
-            if (b2[abs(x - d)] < cnt - u) return false;
-            for (int i = 0; i < u; ++i) p.push_back(x + d);
-            b2[abs(x - d)] -= cnt - u;
-            for (int i = 0; i < cnt - u; ++i) p.push_back(d - x);
+ 
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	ll T; cin >> T;
+    while(T--){
+        ll n; input n;
+        vl vec(n);
+        ll mx = 0;
+        forloop{
+            input vec[i];
+            mx = max(mx, vec[i]);
         }
-        int mn = min(0, *min_element(p.begin(), p.end()));
-        cout << "YES" << '\n';
-        for (int i = 0; i < n; ++i) cout << p[i] - mn << ' ';
-        cout << '\n';
-        cout << d - mn << ' ' << -mn << '\n';
-        return true;
-    };
-    for (int x: d1) {
-        if (ok(abs(x - d2[0]))) return;
+        ll l = 0, r = LLONG_MAX;
+        ll ans = LLONG_MAX;
+        while(l <= r){
+            ll m = (l + r) / 2;
+            ll one = (m + 1) / 2;
+            ll two = m / 2;
+            vl v;
+            forloop{
+                v.push_back(mx - vec[i]);
+            }
+            forloop{
+                ll k = v[i] / 2;
+                v[i] -= min(k, two) * 2;
+                two -= min(k, two);
+            }
+            if(one >= accumulate(v.begin(), v.end(), 0LL)){
+                r = m - 1;
+                ans = min(ans, m);
+            }
+            else{
+                l = m + 1;
+            }
+        }
+        l = 0, r = LLONG_MAX;
+        while(l <= r){
+            ll m = (l + r) / 2;
+            ll one = (m + 1) / 2;
+            ll two = m / 2;
+            vl v;
+            forloop{
+                v.push_back(mx + 1 - vec[i]);
+            }
+            forloop{
+                ll k = v[i] / 2;
+                v[i] -= min(k, two) * 2;
+                two -= min(k, two);
+            }
+            if(one >= accumulate(v.begin(), v.end(), 0LL)){
+                r = m - 1;
+                ans = min(ans, m);
+            }
+            else{
+                l = m + 1;
+            }
+        }
+        print ans << "\n";
     }
-    for (int x: d1) {
-        if (ok(x + d2[0])) return;
-    }
-    cout << "NO\n";
 }
