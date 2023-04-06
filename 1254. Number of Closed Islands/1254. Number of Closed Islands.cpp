fdb12d7ce97d9fcb23f6b9c551774335d0c544a3
Devendra Uraon <devendra123world@gmail.com>
2023-04-06 18:09:46 +0530

    Add files via upload

---
+++ 1254. Number of Closed Islands/1254. Number of Closed Islands.cpp
@@ -0,0 +1,14 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = nums[0];
+        long long sum = nums[0];
+        for(int i = 1; i < n; i++){
+            sum += nums[i];
+            ans = max(ans, (sum + i) / (i + 1));
+        }
+        return ans;
+    }
+};
---
+++ 1254. Number of Closed Islands/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/number-of-closed-islands/">1254. Number of Closed Islands</a>
