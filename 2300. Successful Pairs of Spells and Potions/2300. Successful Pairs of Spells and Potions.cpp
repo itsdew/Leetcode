506d63ebe8f12e27a61db175f33d9b2724e3af2e
Devendra Uraon <devendra123world@gmail.com>
2023-04-13 00:22:20 +0530

    Add files via upload

---
+++ 2300. Successful Pairs of Spells and Potions/2300. Successful Pairs of Spells and Potions.cpp
@@ -0,0 +1,19 @@
+//Author: Devendra Uraon
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& v1, vector<int>& v2, long long success) {
+        sort(v2.begin(), v2.end());
+        int n = v1.size(), m = v2.size();
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            long long req = (success + v1[i] - 1) / v1[i];
+            if (req > v2[m - 1]) {
+                ans.push_back(0);
+                continue;
+            }
+            auto it = lower_bound(v2.begin(), v2.end(), req);
+            ans.push_back(v2.end() - it);
+        }
+        return ans;
+    }
+};
---
+++ 2300. Successful Pairs of Spells and Potions/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/successful-pairs-of-spells-and-potions/">2300. Successful Pairs of Spells and Potions</a>
