cfc2d3883f18916b90b822ba27e0f1cbe1eca5aa
Devendra Uraon <devendra123world@gmail.com>
2023-04-14 18:16:15 +0530

    Add files via upload

---
+++ 516. Longest Palindromic Subsequence/516. Longest Palindromic Subsequence.cpp
@@ -0,0 +1,19 @@
+//Author: Devendra Uraon
+class Solution {
+public:    
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        vector<int> pre(s.size()+1, 0), cur(s.size()+1, 0);
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 1; j <= s.size(); j++){
+                if(s[i-1] == s2[j-1]) cur[j] = 1 + pre[j-1];
+                else cur[j] = max(pre[j], cur[j-1]);
+            }
+            pre = cur;
+        }
+        return pre[s.size()];
+    }
+};
+
+
---
+++ 516. Longest Palindromic Subsequence/README.md
@@ -0,0 +1,2 @@
+## Question : 
+<h2> <a href="https://leetcode.com/problems/longest-palindromic-subsequence/">516. Longest Palindromic Subsequence</a>
